e11b5cc940d4204fdf9fa2a5db2912eecddb2159
Tatou <enzoberti@protonmail.com>
2026-02-01 11:43:56 +0100

    Add random client spawning

--- source/levels/level.gd
+++ source/levels/level.gd
@@ -3,13 +3,20 @@ extends Node2D
 enum states { START, MASK, CUCUMBER, FINISH }
 var state_machine : states
 
+var clients_resources : Array[Resource]
+
 var is_there_client : bool = false
 @onready var spawn_pos : Vector2 = $SpawnPoint.position
-
+var rng = RandomNumberGenerator.new()
 
 ###### BUILT-IN FUNCTIONS ######
 
 func _ready() -> void:
+	clients_resources = [preload("res://characters/cupcake/cupcake_client.tscn"), 
+preload("res://characters/duck/duck_client.tscn"), preload("res://characters/flower/flower_client.tscn"), 
+preload("res://characters/troll/troll_client.tscn"), preload("res://characters/wrestler/wrestler_client.tscn")]
+	
+	
 	spawn_client()
 	
 	state_machine = states.START
@@ -25,7 +32,7 @@ func _process(delta: float) -> void:
 
 #Spawn new client
 func spawn_client() -> void:
-	var client = preload("res://characters/duck/duck_client.tscn")
+	var client = clients_resources[rng.randi_range(0, clients_resources.size() - 1)]
 	var instance = client.instantiate()
 	add_child(instance)
 	instance.position = spawn_pos
